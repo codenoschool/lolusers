a95b9eab894fabc793c4d6ecafb58753d47ce22f
codenoschool <codenoschool@gmail.com>
2018-04-11 21:59:08 -0500

    Request to get users added.

--- WebSite/app.js
+++ WebSite/app.js
@@ -1,12 +1,24 @@
 var express = require('express');
+var request = require('request');
 var path = require('path');
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+var hostRequest = 'http://127.0.0.1:5000/api/users';
+
 app.get('/', (req, res) => {
 	res.render('index');
+});
+
+app.get('/users', (req, res) => {
+	request(hostRequest, { json: true }, (err, response, body) => {
+		if (err) { console.log(err) };
+		res.render('users', {
+			users: body
+		});
+	});
 });
 
 app.listen(3000, () => {
