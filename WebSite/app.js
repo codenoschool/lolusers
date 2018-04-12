99c710ea723a214e9c0820a110a3f0396b8b633c
codenoschool <codenoschool@gmail.com>
2018-04-11 21:02:27 -0500

    WebSite with EJS as view engine.

--- WebSite/app.js
+++ WebSite/app.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var path = require('path');
 var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
+
+app.get('/view', (req, res) => {
+	res.render('index');
+})
 
 app.listen(3000, () => {
 	console.log('* Running on http://127.0.0.1:3000/ (Press CTRL + C to quit)');
