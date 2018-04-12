9c1d7063ec7d80451c3f9f1bf7ae7a9e9107adb5
codenoschool <codenoschool@gmail.com>
2018-04-11 21:15:30 -0500

    Files head, header and footer added.

--- WebSite/app.js
+++ WebSite/app.js
@@ -6,12 +6,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
-app.get('/view', (req, res) => {
 	res.render('index');
-})
+});
 
 app.listen(3000, () => {
 	console.log('* Running on http://127.0.0.1:3000/ (Press CTRL + C to quit)');
